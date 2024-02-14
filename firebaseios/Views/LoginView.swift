6b3bc97149a4a16c6ffcfadced465a5d14f7fdac
Manuel Cazalla <manuelcm23@hotmail.com>
2024-02-14 18:53:49 +0100

    Firebase

--- firebaseios/Views/LoginView.swift
+++ firebaseios/Views/LoginView.swift
@@ -68,6 +68,10 @@ struct LoginView: View {
                          .foregroundColor(.white)
                          .cornerRadius(8)
                  }
+                
+                Button("Crash Now") {
+                    fatalError("Crash was triggered")
+                }
              }
              .padding(.top)
              .alert(isPresented: $showAlert) {
