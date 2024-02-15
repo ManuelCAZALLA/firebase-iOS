917c751c63c6fff096745514aa852759d26fafe6
Manuel Cazalla <manuelcm23@hotmail.com>
2024-02-15 18:50:08 +0100

    Configuramos notificaciones push

--- firebaseios/App/firebaseiosApp.swift
+++ firebaseios/App/firebaseiosApp.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 import Firebase
 import FirebaseCore
+import UserNotifications
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        // Notificaciones push
+        UNUserNotificationCenter.current().delegate = self
         
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: { _, _ in})
+        
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+         
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Enlazamos con APN
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notificatiions: \(error)")
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        // Mandamos nuestro FCM Token a backend
+        
+        Messaging.messaging().subscribe(toTopic: "MCC") { error in
+            print("Error: \(String(describing: error))")
+        }
+        
+        let dataDic: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDic)
     }
 }
 
